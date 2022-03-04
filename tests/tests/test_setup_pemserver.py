de0632bb8fb8303d17bea659c3d0b1ba669dc002
vincentp7212 <86248040+vincentp7212@users.noreply.github.com>
2022-03-04 12:35:32 -0600

    Adding additional tests
    Check if an HTTP server is running and listening on the right TCP port (PEM web interface)
    
    Check if PEM database instance is running

--- tests/tests/test_setup_pemserver.py
+++ tests/tests/test_setup_pemserver.py
@@ -1,11 +1,7 @@
 import pytest
 
 from conftest import (
-    load_ansible_vars,
-    get_hosts,
-    get_os,
     get_pg_type,
-    get_primary,
     get_pemserver,
     get_pg_version,
     get_pg_unix_socket_dir
@@ -55,10 +51,10 @@ def test_setup_pemserver_service():
     service = 'pemagent'
 
     assert host.service(service).is_running, \
-        "Postgres service not running"
+        "Pemagent service not running"
 
     assert host.service(service).is_enabled, \
-        "Postgres service not enabled"
+        "Pemagent service not enabled"
 
 def test_setup_pemserver_pg():
     if get_pg_type() != 'PG':
@@ -88,3 +84,47 @@ def test_setup_pemserver_epas():
     for package in packages:
         assert host.package(package).is_installed, \
             "Package %s not installed" % packages
+
+def test_setup_pemserver_creation():
+    pg_user = 'postgres'
+    pg_group = 'postgres'
+
+    if get_pg_type() == 'EPAS':
+        pg_user = 'enterprisedb'
+        pg_group = 'enterprisedb'
+
+    host= get_pemserver()
+    socket_dir = get_pg_unix_socket_dir()
+    
+    with host.sudo(pg_user):
+        query = r"\list"
+        cmd = host.run('psql -At -h %s -c "%s" postgres | grep pem' % (socket_dir, query))
+        result = cmd.stdout.strip()
+
+    assert len(result) > 0, \
+        "PEM database was not succesfully created"
+
+def test_setup_pemserver_socket():
+    host = get_pemserver()
+    assert host.socket('tcp://0.0.0.0:8443').is_listening, \
+        "PEM web interface is not listening on %s" % '0.0.0.0:8443'
+
+def test_setup_pemserver_web_interface():
+    pg_user = 'postgres'
+    pg_group = 'postgres'
+
+    if get_pg_type() == 'EPAS':
+        pg_user = 'enterprisedb'
+        pg_group = 'enterprisedb'
+
+    address = str(get_pemserver()).strip("<>").split('//')[1]
+    host = get_pemserver()
+    
+    with host.sudo(pg_user):
+        cmd = host.run(" curl --insecure -L -I https://%s/pem | grep HTTP" % address)
+        result = cmd.stdout.strip()
+
+    # Getting status code after filtering out redirection
+    result = result.split('\n')
+    assert '200' in result[len(result)-1], \
+        "PEM web interface is not up and running."
