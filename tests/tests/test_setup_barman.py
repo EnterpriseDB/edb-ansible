999e22bff83c264adc2d4e6d455d53bd5d5cbeea
vincentp7212 <86248040+vincentp7212@users.noreply.github.com>
2022-04-11 11:11:42 -0500

    setup_barman WIP
    Centos7/Debian10 Error with WAL file

---
+++ tests/tests/test_setup_barman.py
@@ -0,0 +1,71 @@
+import pytest
+
+from conftest import (
+    load_ansible_vars,
+    get_pg_type,
+    get_barmanserver,
+    get_pg_version,
+    get_primary,
+    get_pg_unix_socket_dir
+)
+
+def test_setup_barman_backup():
+    ansible_vars = load_ansible_vars()
+    barman_user = ansible_vars['barman_user']
+    host = get_barmanserver()
+
+    with host.sudo(barman_user):
+        cmd = host.run('barman list-server')
+        result = cmd.stdout.strip()
+    
+    assert "primary1" in result, \
+        "%s backup server was not configured correctly" % "primary1"
+
+def test_setup_barman_home_dir():
+    ansible_vars = load_ansible_vars()
+    barman_user = ansible_vars['barman_user']
+    barman_home = ansible_vars['barman_home']
+    host = get_barmanserver()
+
+    with host.sudo(barman_user):
+        cmd = host.run('barman show-server primary1-main | grep barman_home')
+        result = cmd.stdout.strip()
+    
+    assert barman_home in result, \
+        "%s directory was not configured correctly" % barman_home
+
+def test_setup_barman_status():
+    ansible_vars = load_ansible_vars()
+    barman_user = ansible_vars['barman_user']
+    barman_home = ansible_vars['barman_home']
+    host = get_barmanserver()
+
+    with host.sudo(barman_user):
+        cmd = host.run('barman status primary1-main | grep Active')
+        result = cmd.stdout.strip()
+    
+    assert "True" in result, \
+        "primary1-main's server status is not active"
+
+def test_setup_barman_user():
+    ansible_vars = load_ansible_vars()
+    barman_user = ansible_vars['barman_user']
+    host = get_primary()
+
+    pg_user = 'postgres'
+    if get_pg_type() == 'EPAS':
+        pg_user = 'enterprisedb'
+
+    host= get_primary()
+    socket_dir = get_pg_unix_socket_dir()
+    
+    with host.sudo(pg_user):
+        query = "Select * from pg_user where usename = '%s'" % barman_user
+        cmd = host.run('psql -At -h %s -c "%s" postgres' % (socket_dir, query))
+        result = cmd.stdout.strip()
+    
+    assert len(result) > 0, \
+        "Barman user does not exist in primary database"
+
+
+
