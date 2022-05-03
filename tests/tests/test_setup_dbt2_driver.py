70080b1e39bfbe8345f0a0c31e1f9c8f169173e1
vincentp7212 <86248040+vincentp7212@users.noreply.github.com>
2022-05-03 07:36:30 -0500

    Setup_dbt2_driver testing framework (#331)

---
+++ tests/tests/test_setup_dbt2_driver.py
@@ -0,0 +1,34 @@
+import pytest
+
+from conftest import (
+    load_ansible_vars,
+    get_pg_type,
+    get_dbt2_driver,
+    get_pg_version,
+    get_primary,
+    get_pg_unix_socket_dir
+)
+
+def test_setup_dbt2_driver_packages():
+    host = get_dbt2_driver()[0]
+    packages = [
+        'dbt2-driver',
+        'dbt2-exec',
+        'dbt2-scripts'
+    ]
+
+    for package in packages:
+        assert host.package(package).is_installed, \
+            "Package %s not installed" % packages
+
+def test_setup_dbt2_driver_sudo():
+    host = get_dbt2_driver()[0]
+    ansible_vars = load_ansible_vars()
+    dbt2_user = ansible_vars['dbt2_user']
+
+    with host.sudo(dbt2_user):
+        cmd = host.run('env | grep USER')
+        result = cmd.stdout.strip()
+   
+    assert dbt2_user in result, \
+        "%s could not be used as a sudo user" % dbt2_user
