243c15968e27d720fa4ba06e41fd8bd255612a32
Mark Wong <mark.wong@enterprisedb.com>
2022-06-15 00:45:20 -0700

    Add playbooks for DBT-3 (#338)

---
+++ tests/tests/test_setup_dbt3.py
@@ -0,0 +1,40 @@
+import pytest
+
+from conftest import (
+    load_ansible_vars,
+    get_pg_type,
+    get_pg_version,
+    get_primary,
+    get_pg_unix_socket_dir
+)
+
+def test_setup_dbt3_packages():
+    host = get_primary()
+    packages = ['dbt3']
+
+    for package in packages:
+        assert host.package(package).is_installed, \
+            "Package %s not installed" % packages
+
+def test_setup_dbt3_user():
+    ansible_vars = load_ansible_vars()
+    pg_user = 'postgres'
+    pg_group = 'postgres'
+    pg_port = '5432'
+    postgres_user = ansible_vars['postgres_user']
+
+
+    if get_pg_type() == 'EPAS':
+        pg_user = 'enterprisedb'
+        pg_port = '5444'
+        pg_group = 'enterprisedb'
+
+    host = get_primary()
+    socket_dir = get_pg_unix_socket_dir()
+    with host.sudo(pg_user):
+        query = "Select * from pg_user WHERE usename='%s'" % postgres_user
+        cmd = host.run('psql -p %s -At -h %s -c "%s" postgres' % (pg_port, socket_dir, query))
+        result = cmd.stdout.strip()
+    
+    assert len(result) > 0, \
+        "%s was not sucessfully created." % postgres_user
