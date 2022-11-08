42e2a05ca9fea56989a6f2861b6839b39e6469f8
hannahms <36494235+hannahms@users.noreply.github.com>
2022-11-08 12:01:59 -0500

    remove binary copy tasks and update test accordingly

--- tests/tests/test_manage_pgpool2.py
+++ tests/tests/test_manage_pgpool2.py
@@ -21,13 +21,19 @@ def test_manage_pgpool_pcp_user():
         pg_user = 'enterprisedb'
         pg_group = 'enterprisedb'
 
+    pcp_command = 'pcp_node_info'
+
+    if get_pg_type() == 'EPAS':
+        if os_family() == 'Debian':
+            pcp_command = '/usr/edb/pgpool4.3/bin/pcp_node_info'
+
     host = get_pgpool2()[0]
 
     with host.sudo(pg_user):
         cmd = host.run("touch ~/.pcppass")
         cmd = host.run("echo 'localhost:9898:%s:%s' >> ~/.pcppass" % (pcp_user, pcp_pass))
         cmd = host.run("chmod 600 ~/.pcppass ")
-        cmd = host.run("pcp_node_info -U %s -h localhost -w" % pcp_user)
+        cmd = host.run("%s -U %s -h localhost -w" % (pcp_command, pcp_user))
         result = cmd.stdout.strip()
 
     assert len(result) > 0, \
@@ -45,10 +51,16 @@ def test_manage_pgpool_pcp_node_count():
         pg_user = 'enterprisedb'
         pg_group = 'enterprisedb'
 
+    pcp_command = 'pcp_node_count'
+
+    if get_pg_type() == 'EPAS':
+        if os_family() == 'Debian':
+            pcp_command = '/usr/edb/pgpool4.3/bin/pcp_node_count'
+
     host = get_pgpool2()[0]
 
     with host.sudo(pg_user):
-        cmd = host.run("pcp_node_count -U %s -h localhost -w" % pcp_user)
+        cmd = host.run("%s -U %s -h localhost -w" % (pcp_command, pcp_user))
         result = cmd.stdout.strip()
 
     assert result == '1', \
