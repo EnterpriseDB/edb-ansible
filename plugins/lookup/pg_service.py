e21f30abc4733d1b2a23873a69e5eea6e621f774
hannahms <36494235+hannahms@users.noreply.github.com>
2023-01-26 19:59:07 -0500

    update pg_service lookup to include Debian

--- plugins/lookup/pg_service.py
+++ plugins/lookup/pg_service.py
@@ -4,7 +4,7 @@ __metaclass__ = type
 DOCUMENTATION = """
     name: pg_service
     author: Julien Tachoires
-    short_description: Returns systemd service name for PostgreSQL/EPAS on RHEL
+    short_description: Returns systemd service name for PostgreSQL/EPAS on RHEL and Debian
 """
 
 EXAMPLES = """
@@ -22,12 +22,21 @@ class LookupModule(LookupBase):
         pg_type = variables.get('pg_type', '13')
         pg_version = variables.get('pg_version', 'PG')
         pg_instance_name = variables.get('pg_instance_name', 'main')
+        ansible_os_family = variables.get('ansible_os_family', 'RedHat')
 
-        if pg_type == 'EPAS':
-            p = 'edb-as-%s'
-        elif pg_type == 'PG':
-            p = 'postgresql-%s'
-        if pg_instance_name != 'main':
-            return [(p + '-%s') % (pg_version, pg_instance_name)]
-        else:
-            return [p % pg_version]
+        if ansible_os_family == 'RedHat':
+            if pg_type == 'EPAS':
+                p = 'edb-as-%s'
+            elif pg_type == 'PG':
+                p = 'postgresql-%s'
+            if pg_instance_name != 'main':
+                return [(p + '-%s') % (pg_version, pg_instance_name)]
+            else:
+                return [p % pg_version]
+
+        elif ansible_os_family == 'Debian':
+            if pg_type == 'EPAS':
+                p = 'edb-as%s-%s'
+            elif pg_type == 'PG':
+                p = 'postgresql@%s-%s'
+            return [p % (pg_version, pg_instance_name)]
